dea6a85abf87491fecb96de3c52862bab1104e44
violaine-drt <viodarrent@gmail.com>
2024-04-16 12:27:48 +0200

    Ajotu lien vers userwall

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -29,7 +29,8 @@
                     <div class="flex-1">
                         <div class="flex justify-between items-center">
                             <div>
-                                <span class="text-gray-800">{{ $post->user->name }}</span>
+    
+                                <a href="/userwall/{{ $post->user->id}}" class="text-gray-800">{{ $post->user->name }}</a>
                                 <small class="ml-2 text-sm text-gray-600">{{ $post->created_at->format('j M Y, g:i a') }}</small>
                                 @unless ($post->created_at->eq($post->updated_at))
                                     <small class="text-sm text-gray-600"> &middot; {{ __('edited') }}</small>
