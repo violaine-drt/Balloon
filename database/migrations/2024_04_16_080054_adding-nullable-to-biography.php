a76c01d9e973db9c96d9da191f2f63d51e0877d2
Mathurin Sekine <114408539+lemathurin@users.noreply.github.com>
2024-04-17 10:36:44 +0000

    making it look nice 👨‍🎨

--- database/migrations/2024_04_16_080054_adding-nullable-to-biography.php
+++ database/migrations/2024_04_16_080054_adding-nullable-to-biography.php
@@ -24,7 +24,7 @@ return new class extends Migration
     {
         //
         Schema::table('users', function (Blueprint $table) {
-            $table->text('biography')->nullable(false)->change();
+            $table->text('biography')->nullable()->change();
     });
     }
 };
--- resources/views/userwall.blade.php
+++ resources/views/userwall.blade.php
@@ -21,51 +21,57 @@
 
 <x-app-layout>
     <div class="max-w-2xl mx-auto p-4 sm:p-6 lg:p-8">
-        
-        <div class="flex flex-col gap-5">
-            <p class="text-5xl font-bold">{{ $name }}</p>
-            <p class="text-xl font-semibold">{{ $biography }}</p>
-<!--Test affichage nb followers-->   
-            <p class="text-xl font-semibold">{{ $user->followers()->count() }} followers</p>
-            <p class="text-xl font-semibold">this users follows  {{ $user->followings()->count() }}  persons</p>
-
-<!--Afficher les messages de session définis dans le controller -->     
-    @if(session()->has('success'))
-    <div class="alert alert-success">
-        {{ session('success') }}
-    </div>
-    @endif
-         <!--On ne peut follow/unfollow que les autres utilisateurs-->     
-         @if (!$user->is(auth()->user()))
-         <div class="mt-4 space-x-2">
-            <!--Condition pour afficher follow ou unfollow--> 
-             @if(auth()->user()->follows($user))
-             <form method="POST" action="{{ route('users.unfollow',$user ->id)}}">
-                 @csrf
-             <x-primary-button>{{ __('Unfollow') }}</x-primary-button>
-             </form>
-
-             @else
-             <form method="POST" action="{{ route('users.follow',$user ->id)}}">
-                 @csrf
-             <x-primary-button>{{ __('Follow') }}</x-primary-button>
-             </form>
-             @endif
-         </div>
-         @endif
+        <div class="mt-6 bg-white shadow-sm rounded-lg divide-y">
+            <div class="p-6 flex space-x-2">
+                <div class="flex gap-3 w-full justify-between">
+                    <div class="flex flex-col gap-2 mr-10">
+                        <p class="text-5xl font-bold">{{ $name }}</p>
+                        <p class="text-xl font-semibold">{{ $biography }}</p>
+                    </div>
+                        <!--Test affichage nb followers-->   
+                    <div class="flex flex-col gap-1 items-end">
+                        @if (!$user->is(auth()->user()))
+                            <div class="mb-3 space-x-2">
+                                <!--Condition pour afficher follow ou unfollow--> 
+                                @if(auth()->user()->follows($user))
+                                <form method="POST" action="{{ route('users.unfollow',$user ->id)}}">
+                                    @csrf
+                                <x-primary-button>{{ __('Unfollow') }}</x-primary-button>
+                                </form>
+                                @else
+                                <form method="POST" action="{{ route('users.follow',$user ->id)}}">
+                                    @csrf
+                                <x-primary-button>{{ __('Follow') }}</x-primary-button>
+                                </form>
+                                @endif
+                            </div>         
+                        @endif
+                        <p class="text-lg text-gray-600">{{ $user->followers()->count() }} followers</p>
+                        <p class="text-lg text-gray-600">{{ $user->followings()->count() }} following</p>
+                        {{-- @if(session()->has('success'))
+                        <div class="alert alert-success">
+                            {{ session('success') }}
+                        </div>
+                        @endif --}}
+                        <!--On ne peut follow/unfollow que les autres utilisateurs-->     
+                        
+                    </div>
         </div>
+    </div>
+    </div>
         <div class="mt-6 bg-white shadow-sm rounded-lg divide-y">
             @foreach ($userPosts as $post)
                 <!-- Afficher le post -->
                     <div class="p-6 flex space-x-2">
-                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-600 -scale-x-100" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z" />
-                        </svg>
+                        
                         <div class="flex-1">
                             <div class="flex justify-between items-center">
-                                <div>
+                                <div class="flex gap-2 items-center">
+                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-600 -scale-x-100" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z" />
+                                    </svg>
                                     <span class="text-gray-800">{{ $post->user->name }}</span>
-                                    <small class="ml-2 text-sm text-gray-600">{{ $post->created_at->format('j M Y, g:i a') }}</small>
+                                    <small class="text-sm text-gray-600">{{ $post->created_at->format('j M Y, g:i a') }}</small>
                                     @unless ($post->created_at->eq($post->updated_at))
                                         <small class="text-sm text-gray-600"> &middot; {{ __('edited') }}</small>
                                     @endunless
