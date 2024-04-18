9a22c652befca1ee3a04aa04f8905c8d52318bbe
violaine-drt <viodarrent@gmail.com>
2024-04-18 14:49:49 +0200

    modif navbar

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -73,11 +73,11 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
-            </x-responsive-nav-link>
             <x-responsive-nav-link :href="route('posts.index')" :active="request()->routeIs('posts.index')">
-                {{ __('Posts') }}
+                {{ __('Feed') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('userwall',auth()->user()->id)" :active="request()->routeIs('userwall')">
+                {{ __('Profile') }}
             </x-responsive-nav-link>
         </div>
 
@@ -90,7 +90,7 @@
 
             <div class="mt-3 space-y-1">
                 <x-responsive-nav-link :href="route('profile.edit')">
-                    {{ __('Profile') }}
+                    {{ __('Settings') }}
                 </x-responsive-nav-link>
 
                 <!-- Authentication -->
